4c47200d036f37f6d9cac26108db51ccaae2128b
Artem Grachov <grachov2195@gmail.com>
2017-07-20 10:38:56 +0300

    Google maps. Circles + InfoWindow with test value

--- app/news/post/post.component.js
+++ app/news/post/post.component.js
@@ -25,9 +25,105 @@ angular.module('post')
                     })
 
             }
-            ctrl.map = {
-                center: { latitude: 45, longitude: -73 },
-                zoom: 8
+            ctrl.testMap = {
+                center: {
+                    latitude: 48.698198,
+                    longitude: 26.575646,
+                },
+                zoom: 12
             }
+            ctrl.circleTemplate = {
+                radius: 500,
+                stroke: {
+                    color: 'green',
+                    weight: 1,
+                    opacity: .8
+                },
+                fill: {
+                    color: 'blue',
+                    opacity: .1
+                },
+                events: {
+                    click: function(e, event, scope) {
+                        console.log(arguments);
+                        let testWindow = new google.maps.InfoWindow({
+                            content: `Test value is: <b>${scope.control.testVal}</b>`,
+                            position: { lat: this.center.latitude, lng: this.center.longitude },
+                            map: e.map
+                        })
+                    }
+                }
+            }
+            ctrl.testCircles = [{
+                    center: {
+                        latitude: 48.708198,
+                        longitude: 26.605646,
+                    },
+                    testVal: 1
+                },
+                {
+                    center: {
+                        latitude: 48.718198,
+                        longitude: 26.575646,
+                    },
+                    testVal: 2
+                },
+                {
+                    center: {
+                        latitude: 48.698198,
+                        longitude: 26.595646,
+                    },
+                    testVal: 3
+                },
+                {
+                    center: {
+                        latitude: 48.709198,
+                        longitude: 26.559646,
+                    },
+                    testVal: 4
+                },
+                {
+                    center: {
+                        latitude: 48.688198,
+                        longitude: 26.605646,
+                    },
+                    testVal: 5
+                },
+                {
+                    center: {
+                        latitude: 48.678198,
+                        longitude: 26.545646,
+                    },
+                    testVal: 6
+                },
+                {
+                    center: {
+                        latitude: 48.688198,
+                        longitude: 26.555646,
+                    },
+                    testVal: 7
+                },
+                {
+                    center: {
+                        latitude: 48.708198,
+                        longitude: 26.575646,
+                    },
+                    testVal: 8
+                },
+                {
+                    center: {
+                        latitude: 48.718198,
+                        longitude: 26.555646,
+                    },
+                    testVal: 9
+                },
+                {
+                    center: {
+                        latitude: 48.728198,
+                        longitude: 26.535646,
+                    },
+                    testVal: 10
+                }
+            ]
         }
     })
